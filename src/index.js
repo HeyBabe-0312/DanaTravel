2648f8552691466149fbafec29745978db225819
[MISUMI| Hieu HoMinh <minhhieu.v73i.ho@misumi.co.jp>
2025-05-26 00:20:44 +0900

    fix: Update axios base URL to use the production API endpoint

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import axios from "axios";
 
 // axios.defaults.baseURL = "https://danatravel-be.vercel.app/";
-axios.defaults.baseURL = "http://localhost:5001/api";
+axios.defaults.baseURL = "https://dana-travel-backend.vercel.app/api";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
